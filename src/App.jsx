b1bebb87d003b7c659158fbde7c35cd6051a74ff
Muhammad <muhammadfabil40@gmail.com>
2025-02-18 21:06:24 +0700

    .

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+'use client'
+
+
+import PWABadge from "./PWABadge.jsx";
+import "./App.css";
+import SplashCursor from "../SplashCursor/SplashCursor.jsx";
+
+function App() {
+  
+
+  return (
+    <div className="bg-black">
+    <h1>halo</h1>
+    <SplashCursor />
+    </div>
+  );
+}
+
+export default App;
