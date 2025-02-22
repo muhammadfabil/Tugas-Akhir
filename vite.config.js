3e5adb487899ee935a72f5fbf9802b155c87d658
Muhammad <muhammadfabil40@gmail.com>
2025-02-22 20:02:30 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,20 +13,57 @@ export default defineConfig({
       injectRegister: false,
 
       manifest: {
-        name: 'Tugas-Akhir',
-        short_name: 'Tugas-Akhir',
-        description: 'Tugas-Akhir',
-        theme_color: '#ffffff',
+        name: 'Sistem Antrian Pelayanan Prodi & Administrasi',
+        short_name: 'AntrianProdi',
+        description: 'Web App PWA untuk sistem antrian pelayanan administrasi dan bimbingan dosen',
+        theme_color: '#1277C9',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
       },
 
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
         cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'html-cache',
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources',
+            },
+          },
+        ],
         clientsClaim: true,
       },
 
       devOptions: {
-        enabled: false,
+        enabled: true,
         navigateFallback: 'index.html',
         suppressWarnings: true,
         type: 'module',
